15114b5d8aa220b521fb9e7c5e3acf87bde4ffb5
nelsonic <nelson+github@dwyl.com>
2022-10-21 15:09:51 +0100

    remember to run mix assets.deploy to create the required JS & CSS files see: https://github.com/dwyl/learn-alpine.js/pull/5#issuecomment-1287020097

--- mix.exs
+++ mix.exs
@@ -62,7 +62,8 @@ defmodule App.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"],
+      s: ["phx.server"]
     ]
   end
 end
