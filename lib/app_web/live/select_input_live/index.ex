c620d03e63141e3bd2cf1e908e589b594d2df819
SimonLab <simon@dwyl.com>
2022-11-09 14:29:30 +0000

    Filter list of people
    - Use phx-keyup event to filter the list of people
    - Replace Alpine.js @ shortcut with x-on as it breaks the app.js
    when using LiveView

--- lib/app_web/live/select_input_live/index.ex
+++ lib/app_web/live/select_input_live/index.ex
@@ -2,8 +2,19 @@ defmodule AppWeb.SelectInputLive.Index do
   use AppWeb, :live_view
   alias App.Tasks
 
+  @impl true
   def mount(_params, _session, socket) do
     people = Tasks.list_people()
     {:ok, assign(socket, :people, people)}
+  end
+
+  @impl true
+  def handle_event("filter-items", %{"key" => _key, "value" => value}, socket) do
+    people =
+      Tasks.list_people()
+      |> Enum.filter(fn p -> String.contains?(p.name, value) end)
+
+
+    {:noreply, assign(socket, :people, people)}
   end
 end
