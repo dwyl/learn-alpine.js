8a30ce817b418534fdca5477c4dd05ccff8ac6c8
SimonLab <simon@dwyl.com>
2022-11-16 12:54:52 +0000

    Create person for test
    Create person before using the toggle event

--- test/app_web/live/select_input_test.exs
+++ test/app_web/live/select_input_test.exs
@@ -2,6 +2,7 @@ defmodule AppWeb.SelectInpuLiveTest do
   use AppWeb.ConnCase
 
   import Phoenix.LiveViewTest
+  alias App.Tasks.Person
 
   describe "Index" do
     test "Display select input", %{conn: conn} do
@@ -16,8 +17,15 @@ defmodule AppWeb.SelectInpuLiveTest do
     end
 
     test "handle_event toggle", %{conn: conn} do
+      person = %Person{
+        name: "Bob",
+        picture: "https://avatars.githubusercontent.com/u/22345430?v=4",
+        selected: false
+      }
+
+      p = App.Repo.insert!(person)
       {:ok, view, _html} = live(conn, Routes.select_input_index_path(conn, :index))
-      assert render_hook(view, "toggle", %{"id" => "1"})
+      assert render_hook(view, "toggle", %{"id" => "#{p.id}"})
     end
   end
 end
